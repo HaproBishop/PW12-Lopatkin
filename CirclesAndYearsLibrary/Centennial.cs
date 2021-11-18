5fb28740f870c17c0df77beaadaf06302ddeebf9
HaproBishop <haprobishop@yandex.ru>
2021-11-18 21:20:17 +0300

    Разработан класс для отображения столетия

---
+++ CirclesAndYearsLibrary/Centennial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CirclesAndYearsLibrary
+{
+    public class Centennial
+    {
+        string _errorinfo = "Год не может быть отрицательным!";
+        int _year;
+        public int Year { get => _year; set => _year = ProveValue(value) ? value : throw new Exception(_errorinfo); }
+        public Centennial() { }
+        public Centennial(int year)
+        {
+            Year = year;
+        }
+        private bool ProveValue(double value)
+        {
+            if (value >= 0) return true;
+            return false;
+        }
+        public int DisplayCentennial()
+        {
+            if (_year % 100 >= 1) return _year / 100 + 1;
+            else return _year / 100;
+        }
+        public int DisplayCentennial(int year)
+        {
+            Year = year;
+            if (_year % 100 >= 1) return _year / 100 + 1;
+            else return _year / 100;
+        }
+    }
+}
